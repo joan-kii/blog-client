a0cf94f22191593f195d1e0e4107b183811c965a
joan-kii <joan.sb81@gmail.com>
2021-11-30 05:25:21 +0100

    bug footer position

--- components/layout.js
+++ components/layout.js
@@ -33,6 +33,16 @@ const Layout = ({children, home}) => {
             </>
           )}
         </header>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>
+                <span className={styles.arrowLink}>&#10094;</span>
+                <span className={utilStyles.headingMd}>Back to home</span>
+              </a>
+            </Link>
+          </div>
+        )}
         <main>{children}</main>
         {!home && (
           <div className={styles.backToHome}>
